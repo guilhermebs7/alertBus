528b88dfecd1819862e105101066213fc6d00f28
guilhermebs7 <gbs5@cin.ufpe.br>
2026-08-24 16:41:02 -0300

    feat(repository): add UserRepository interface for user data access

---
+++ user-service/src/main/java/alertbus/user_service/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package alertbus.user_service.repository;
+
+import alertbus.user_service.domain.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface UserRepository extends JpaRepository<User, UUID> {
+    Optional<User> findByEmail(String email);
+    boolean existsByEmail(String email);
+
+}
