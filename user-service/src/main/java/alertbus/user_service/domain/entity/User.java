627f9bc7541c30f7d63f2ad27f28ae26f02c8a94
guilhermebs7 <gbs5@cin.ufpe.br>
2026-08-24 16:35:51 -0300

    feat(user): add User entity with fields for id, name, email, and password

---
+++ user-service/src/main/java/alertbus/user_service/domain/entity/User.java
@@ -0,0 +1,29 @@
+package alertbus.user_service.domain.entity;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "tb_users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false,unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+}
