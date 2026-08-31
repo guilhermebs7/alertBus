7bb260fe281456b9825637366694530bb24c1f52
guilhermebs7 <gbs5@cin.ufpe.br>
2026-08-31 15:53:57 -0300

    feat(dto): add validation annotations for UserRequestDTO fields

--- user-service/src/main/java/alertbus/user_service/dto/request/UserRequestDTO.java
+++ user-service/src/main/java/alertbus/user_service/dto/request/UserRequestDTO.java
@@ -1,8 +1,17 @@
 package alertbus.user_service.dto.request;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
 public record UserRequestDTO(
+        @NotBlank
         String name,
+
+        @NotBlank
+        @Email
         String email,
+
+        @NotBlank
         String password
 ) {
 }
