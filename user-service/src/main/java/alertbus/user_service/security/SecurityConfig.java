c75f1ae4533a5905d446429c2dff2676a27fc5c3
guilhermebs7 <gbs5@cin.ufpe.br>
2026-08-31 16:49:34 -0300

    feat(security): restrict access to /users endpoint for ADMIN_COMPANY role

--- user-service/src/main/java/alertbus/user_service/security/SecurityConfig.java
+++ user-service/src/main/java/alertbus/user_service/security/SecurityConfig.java
@@ -30,6 +30,7 @@ public class SecurityConfig {
                         .requestMatchers(HttpMethod.POST, "/users").permitAll()
                         .requestMatchers(HttpMethod.POST, "/users/login").permitAll()
                         .requestMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html").permitAll()
+                        .requestMatchers(HttpMethod.GET,"/users").hasRole("ADMIN_COMPANY")
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
