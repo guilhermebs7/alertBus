a1d917de23bb163de73e46ff3841c10c9559cc3e
guilhermebs7 <gbs5@cin.ufpe.br>
2026-08-31 16:49:10 -0300

    feat(dto): add role field to UserRequestDTO for user role assignment

--- user-service/src/main/java/alertbus/user_service/dto/request/UserRequestDTO.java
+++ user-service/src/main/java/alertbus/user_service/dto/request/UserRequestDTO.java
@@ -1,5 +1,6 @@
 package alertbus.user_service.dto.request;
 
+import alertbus.user_service.domain.entity.UserRole;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 
@@ -12,6 +13,8 @@ public record UserRequestDTO(
         String email,
 
         @NotBlank
-        String password
+        String password,
+
+        UserRole role
 ) {
 }
