437881e0f6dca67ea9d70431d450aa19d07e4c67
guilhermebs7 <gbs5@cin.ufpe.br>
2026-08-31 16:23:33 -0300

    feat(controller): add OpenAPI annotations for UserController endpoints

--- user-service/src/main/java/alertbus/user_service/controller/UserController.java
+++ user-service/src/main/java/alertbus/user_service/controller/UserController.java
@@ -5,6 +5,8 @@ import alertbus.user_service.dto.request.UserRequestDTO;
 import alertbus.user_service.dto.response.LoginResponseDTO;
 import alertbus.user_service.dto.response.UserResponseDTO;
 import alertbus.user_service.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -16,28 +18,33 @@ import java.util.UUID;
 
 @RestController
 @RequestMapping("/users")
+@Tag(name = "User Controller", description = "Endpoint para cadastrar usuários e efetuar o login")
 public class UserController {
 
     @Autowired
     private UserService userService;
 
+    @Operation(summary = "Criar usuário")
     @PostMapping
     public ResponseEntity<UserResponseDTO> createUser(@RequestBody UserRequestDTO dto){
         UserResponseDTO response= userService.createUser(dto);
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
 
+    @Operation(summary = "Busca usuário por ID")
     @GetMapping("/{id}")
     public ResponseEntity<UserResponseDTO> getUserById(@PathVariable UUID id){
         UserResponseDTO response= userService.getUserById(id);
         return ResponseEntity.ok(response);
     }
 
+    @Operation(summary = "Lista todos os usuários")
     @GetMapping
     public ResponseEntity<List<UserResponseDTO>> getAllUsers(){
         List<UserResponseDTO> response = userService.getAllUsers();
         return ResponseEntity.ok(response);
     }
+    @Operation(summary = "Efetua o login")
     @PostMapping("/login")
     public ResponseEntity<LoginResponseDTO> login(@RequestBody LoginRequestDTO dto){
         System.out.println("Tentando login para o email: " + dto.email());
