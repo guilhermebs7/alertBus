55302fd3f94b854cdba9a843f72e9c21e3210295
guilhermebs7 <gbs5@cin.ufpe.br>
2026-08-31 15:53:20 -0300

    fix(service): update JWT issuer in TokenService for consistency

--- user-service/src/main/java/alertbus/user_service/service/TokenService.java
+++ user-service/src/main/java/alertbus/user_service/service/TokenService.java
@@ -38,7 +38,7 @@ public class TokenService {
         try{
             Algorithm algorithm= Algorithm.HMAC256(secret);
             return JWT.require(algorithm)
-                    .withIssuer("alertbus-user0service")
+                    .withIssuer("alert-bus-service")
                     .build()
                     .verify(token)
                     .getSubject();
