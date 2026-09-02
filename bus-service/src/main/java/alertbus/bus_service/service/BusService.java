801cac73f8418208a0c9f42d46fc7635e0a0236d
guilhermebs7 <gbs5@cin.ufpe.br>
2026-09-01 21:27:31 -0300

    feat(service): add methods to retrieve all buses and update bus status

--- bus-service/src/main/java/alertbus/bus_service/service/BusService.java
+++ bus-service/src/main/java/alertbus/bus_service/service/BusService.java
@@ -3,11 +3,14 @@ package alertbus.bus_service.service;
 import alertbus.bus_service.dto.request.BusRequesDTO;
 import alertbus.bus_service.dto.response.BusResponseDTO;
 import alertbus.bus_service.entity.Bus;
+import alertbus.bus_service.entity.BusStatus;
 import alertbus.bus_service.repository.BusRepository;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -30,6 +33,29 @@ public class BusService {
 
         Bus saved = busRepository.save(bus);
         return toResponseDTO(saved);
+    }
+
+    public List<BusResponseDTO> getAllBuses(){
+        return busRepository.findAll().stream()
+                .map(this::toResponseDTO)
+                .toList();
+    }
+    public BusResponseDTO getBusById(Long id){
+        Bus bus= busRepository.findById(id)
+                .orElseThrow(()-> new RuntimeException("Ônibus não encontrado com o ID: "+ id));
+                return toResponseDTO(bus);
+
+    }
+    @Transactional
+    public BusResponseDTO updateBusStatus(Long id, BusStatus newStatus){
+        Bus bus= busRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Ônibus não encontrado com o ID:" + id));
+
+        bus.setStatus(newStatus);
+        Bus updated= busRepository.save(bus);
+        return toResponseDTO(updated);
+
+
     }
 
     private BusResponseDTO toResponseDTO(Bus bus) {
