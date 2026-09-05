dfb8622a0b1b883e4d1bec7e516dfa85d11bd628
guilhermebs7 <gbs5@cin.ufpe.br>
2026-09-04 21:16:24 -0300

    feat(init): initialize ETA service with application configuration and Maven wrapper

---
+++ eta-service/src/main/java/alert/bus/eta_service/EtaServiceApplication.java
@@ -0,0 +1,13 @@
+package alert.bus.eta_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EtaServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EtaServiceApplication.class, args);
+	}
+
+}
